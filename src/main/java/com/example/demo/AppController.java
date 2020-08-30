895c0be62bcc6f1554b96aec0a6f7492f2639acd
micmc23000 <micmc48@gmail.com>
2020-08-30 22:56:00 +0100

    Adding Basic Controller for View All

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping("/products")
+    public ModelAndView getProductsPage() {
+        String viewName = "/allProducts";
+        List<Product> productList = productService.getAllProducts();
+        System.out.println("getProductsPage");
+        System.out.println(productList);
+        return new ModelAndView(viewName, "productList", productList);
+    }
+}
